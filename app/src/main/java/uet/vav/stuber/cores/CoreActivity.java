27373ab128ddb3ef479d82c9671ea244a299990e
Giap Nguyen <nvangiap5894@gmail.com>
2016-04-09 12:13:22 +0700

    Removed git ignore files

--- app/src/main/java/uet/vav/stuber/cores/CoreActivity.java
+++
@@ -1,9 +0,0 @@
-package uet.vav.stuber.cores;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by darkmoonus on 4/9/16.
- */
-public class CoreActivity extends AppCompatActivity {
-}
--- app/src/main/java/uet/vav/stuber/utils/Constants.java
+++
@@ -1,8 +0,0 @@
-package uet.vav.stuber.utils;
-
-/**
- * Created by darkmoonus on 4/9/16.
- */
-public class Constants {
-    public static String BASE_URL = "";
-}
